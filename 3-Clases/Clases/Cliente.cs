78e4efdbab2b9e715708be8cea0d2440a106737f
bilyfer <bily.fernandez@unah.edu.hn>
2018-10-11 07:36:48 -0600

    Agregando ejercicio de clases

---
+++ 3-Clases/Clases/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+
+        public Cliente()
+        {
+            Id = 1;
+        }
+
+        public Cliente(int id, string nombre)
+        {
+            Id = id;
+            Nombre = nombre;
+        }        
+    }
+}
---
+++ 3-Clases/Clases/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clases
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var cliente1 = new Cliente();
+            cliente1.Nombre = "Juan Perez";
+
+            var cliente2 = new Cliente(2, "Maria Martinez");
+
+            MessageBox.Show(cliente1.Id + " " + cliente1.Nombre);
+            MessageBox.Show(cliente2.Id + " " + cliente2.Nombre);
+        }
+    }
+}
