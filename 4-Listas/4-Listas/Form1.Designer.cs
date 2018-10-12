b32f3c3d15bd937c4658c65a454ee0dc0dd729d4
bilyfer <bily.fernandez@unah.edu.hn>
2018-10-11 18:46:39 -0600

    Agregando ejercicio de listas

---
+++ 4-Listas/4-Listas/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace _4_Listas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Lista de Clientes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Lista String";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(275, 54);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(209, 173);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(275, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Llenar Lista";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(615, 54);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(209, 173);
+            this.listBox2.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(508, 61);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = ">";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(508, 103);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = ">>";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(508, 189);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "<<";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(508, 147);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "<";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 261);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ 4-Listas/4-Listas/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_Listas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var cliente1 = new Cliente();
+            cliente1.Id = 1;
+            cliente1.Nombre = "Batman";
+
+            var cliente2 = new Cliente();
+            cliente2.Id = 2;
+            cliente2.Nombre = "Superman";
+
+            var cliente3 = new Cliente();
+            cliente3.Id = 3;
+            cliente3.Nombre = "Aquaman";
+
+            var listadeClientes = new List<Cliente>();
+            listadeClientes.Add(cliente1);
+            listadeClientes.Add(cliente2);
+            listadeClientes.Add(cliente3);
+            
+
+            //for (int i = 0; i <= 2; i++)
+            //{
+            //    MessageBox.Show(listadeClientes[i].Nombre);
+            //}
+
+            foreach (var cliente in listadeClientes)
+            {
+                MessageBox.Show(cliente.Nombre);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var listaNombres = new List<string>();
+            listaNombres.Add("Mujer Maravilla");
+            listaNombres.Add("Linterna Verde");
+            listaNombres.Add("Cyborg");
+
+            foreach (var nombre in listaNombres)
+            {
+                MessageBox.Show(nombre);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var cliente1 = new Cliente();
+            cliente1.Id = 1;
+            cliente1.Nombre = "Batman";
+
+            var cliente2 = new Cliente();
+            cliente2.Id = 2;
+            cliente2.Nombre = "Superman";
+
+            var cliente3 = new Cliente();
+            cliente3.Id = 3;
+            cliente3.Nombre = "Aquaman";
+
+            var listadeClientes = new List<Cliente>();
+            listadeClientes.Add(cliente1);
+            listadeClientes.Add(cliente2);
+            listadeClientes.Add(cliente3);
+
+            foreach (var cliente in listadeClientes)
+            {
+                listBox1.Items.Add(cliente.Id + " - " + cliente.Nombre);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            foreach (var item in listBox1.Items)
+            {
+                listBox2.Items.Add(item);
+            }
+
+            listBox1.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var elementoSeleccionado = listBox1.SelectedItem;
+
+            listBox2.Items.Add(elementoSeleccionado);
+
+            listBox1.Items.Remove(elementoSeleccionado);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            foreach (var item in listBox2.Items)
+            {
+                listBox1.Items.Add(item);
+            }
+
+            listBox2.Items.Clear();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var elementoSeleccionado = listBox2.SelectedItem;
+
+            listBox1.Items.Add(elementoSeleccionado);
+
+            listBox2.Items.Remove(elementoSeleccionado);
+        }
+    }
+}
